285bef691d44f773a1c7ba65288b164b3b2e5ecd
tomdanbi <bohyun611.kim@gmail.com>
2021-11-08 11:47:23 +0900

    삼성티비 볼륨 추상 인터페이스 구현

--- src/main/java/org/cat/danbi/web/core/SamsungTV.java
+++ src/main/java/org/cat/danbi/web/core/SamsungTV.java
@@ -5,4 +5,14 @@ public class SamsungTV implements TV{
     public SamsungTV() {
         System.out.println("SamsungTV is created.");
     }
+
+    @Override
+    public void volumeUp() {
+        System.out.println("삼성티비 볼륨 업");
+    }
+
+    @Override
+    public void volumeDown() {
+        System.out.println("삼성티비 볼륨 다운");
+    }
 }
