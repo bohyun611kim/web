3998c1233d6c0850cca40b047bb6b238112ad472
tomdanbi <bohyun611.kim@gmail.com>
2021-11-08 11:42:48 +0900

    엘지티비 파워 인터페이스 추가

--- src/main/java/org/cat/danbi/web/core/LgTV.java
+++ src/main/java/org/cat/danbi/web/core/LgTV.java
@@ -5,4 +5,16 @@ public class LgTV implements TV {
     public LgTV() {
         System.out.println("LgTV is created.");
     }
+
+
+    @Override
+    public void powerOn() {
+        System.out.println("LgTV is Power On!");
+    }
+
+    @Override
+    public void powerOff() {
+        System.out.println("LgTV is Power Off!");
+
+    }
 }
